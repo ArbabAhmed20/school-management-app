68ca081599225ae72db86daa1e5257678bae2d73
Arbab Ahmed <arbabahmed631@gmail.com>
2026-02-16 14:08:09 +0530

    NavBar Completed and added more components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Image from "next/image";
 
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <div className={"flex items-center justify-between p-4 "}>
+        <div className={"hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2"}>
+            {/*SearchBar*/}
+            <Image src={"/search.png"} alt={"search.png"} width={14} height={14} />
+            <input className={"w-[200px] p-2 bg-transparent outline-none"} placeholder={"Search..."}  type={"text"} />
+        </div>
+        <div className={"flex flex-row items-center gap-6 justify-end w-full"}>
+            {/*IconsAndUser*/}
+            <div className={"bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer"}>
+                <Image src={"/message.png"} alt={"message.png"} width={20} height={20} />
+            </div>
+            <div className={"bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer"}>
+                <Image src={"/announcement.png"} alt={"announcement.png"} width={20} height={20} />
+            </div>
+            <div className={"flex flex-col"}>
+                <span className={"text-xs leading-3 font-medium"}>John Doe</span>
+                <span className={"text-[10px] text-gray-500 text-right"}>Admin</span>
+            </div>
+            <Image src={"/avatar.png"} alt={"avatar.png"} width={36} height={36} className={"rounded-full"} />
+        </div>
+        </div>
     )
 }
 
