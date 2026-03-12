d0dec2171f3aa65ceb0bed95c45e96fe8e052ddd
Arbab Ahmed <arbabahmed631@gmail.com>
2026-03-12 12:24:29 +0530

    Implemented Prisma Database Operations on Postgres

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -3,21 +3,12 @@ import TableSearch from "@/components/TableSearch";
 import Image from "next/image";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
-import Link from "next/link";
 import {role, teachersData} from "@/lib/data";
 import FormModal from "@/components/FormModal";
+import {Class, Subject, Teacher} from "@/generated/prisma/client";
+import {prisma} from "@/lib/prisma";
 
-type Teacher = {
-    id: number;
-    teacherId: number;
-    name: string;
-    email?: string;
-    photo: string;
-    phone: string;
-    subjects: string[];
-    classes: string[];
-    addresses: string[];
-}
+type TeacherList = Teacher & {subjects:Subject[]} & {classes:Class[]}
 
 const columns = [
     {
@@ -55,23 +46,23 @@ const columns = [
     },
 ]
 
-function TeacherListPage() {
+async function TeacherListPage() {
 
-    const renderRow = (item: Teacher) =>{
+    const renderRow = (item: TeacherList) =>{
         return (
             <tr key={item.id} className={"border-b border-gray-200 even:bg-gray-50 text-sm"}>
                 <td className={"flex items-center gap-4 p-4"}>
-                    <Image src={item.photo} alt={"photo"} height={40} width={40} className={"md:hidden xl:block w-10 h-10 rounded-full object-cover"}/>
+                    <Image src={item.img || "/Profile-Male-Transparent.png"} alt={"photo"} height={40} width={40} className={"md:hidden xl:block w-10 h-10 rounded-full object-cover"}/>
                     <div className={"flex flex-col"}>
                         <h3 className={"font-semibold"}>{item.name}</h3>
                         <p className={"text-xs"}>{item?.email}</p>
                     </div>
                 </td>
-                <td className="hidden md:table-cell">{item.teacherId}</td>
-                <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
-                <td className="hidden md:table-cell">{item.classes.join(",")}</td>
+                <td className="hidden md:table-cell">{item.id}</td>
+                <td className="hidden md:table-cell">{item.subjects.map(subject=>subject.name).join(",")}</td>
+                <td className="hidden md:table-cell">{item.classes.map(classes=>classes.name).join(",")}</td>
                 <td className="hidden md:table-cell">{item.phone}</td>
-                <td className="hidden md:table-cell">{item.addresses}</td>
+                <td className="hidden md:table-cell">{item.address}</td>
                 <td>
                     <div className={"flex items-center gap-2"}>
                         {role === "admin" && (
@@ -85,6 +76,14 @@ function TeacherListPage() {
             </tr>
         )
     }
+
+    const  teacher = await prisma.teacher.findMany({
+        include: {
+            subjects:true,
+            classes:true,
+        }
+    });
+    console.log(teacher);
     return (
         <div className={"bg-white p-4 rounded-md flex-1 m-4 mt-0"}>
            <div className={"flex items-center justify-between"}>
@@ -111,7 +110,7 @@ function TeacherListPage() {
                 </div>
             </div>
             </div>
-            <Table columns={columns} renderRow={renderRow} data={teachersData}/>
+            <Table columns={columns} renderRow={renderRow} data={teacher}/>
             <Pagination />
         </div>
     )
